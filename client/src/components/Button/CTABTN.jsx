6051d910f8838d33cc989574f30930bfbefb4614
mazenaldaher <mazen0aldaher@gmail.com>
2024-09-05 19:20:39 +0300

    commit

--- client/src/components/Button/CTABTN.jsx
+++ client/src/components/Button/CTABTN.jsx
@@ -1,23 +1,39 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable react/prop-types */
 /* eslint-disable prettier/prettier */
 /* eslint-disable quotes */
-/* eslint-disable prettier/prettier */
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 import React from "react";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
+const CTABTN = ({ link, title }) => {
+  const theme = useTheme(); // useTheme provides the full theme object
+  const mode = theme.palette.mode;
 
-const CTABTN = ({link,title}) => {
-  return( 
-  <Box sx={{my:2}} >
-  <NavLink to={link}>
-  <Button variant="CTA" sx={{p:2,minWidth:"200px"}}>
-  <Typography  variant="navtext" >
-  {title}
-  </Typography>
-  </Button>
-  </NavLink>
-  </Box>
+  // Define color based on the theme mode (light, dark, autism)
+  const getColorByTheme = () => {
+    switch (mode) {
+      case "light":
+        return "#fff";
+      case "dark":
+        return "#fff";
+      case "autism": // If using a custom theme mode for autism
+        return theme.palette.primary.main;
+      default:
+        return "#fff"; // Default to white if mode is not detected
+    }
+  };
+
+  return (
+    <Box sx={{ my: 2 }}>
+      <NavLink to={link} style={{ textDecoration: "none" }}>
+        <Button variant="CTA" sx={{ p: 2, minWidth: "200px" }}>
+          <Typography variant="navtext" color={getColorByTheme()}>
+            {title}
+          </Typography>
+        </Button>
+      </NavLink>
+    </Box>
   );
 };
 
